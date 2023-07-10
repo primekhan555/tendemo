058f5f2a666b388f2298e1a5d4d178b8d76ca9b9
primekhan555 <primekhan555@gmail.com>
2023-07-10 10:11:32 +0500

    comments removed

--- lib/presentation/views/watch_section/watch_component.dart
+++ lib/presentation/views/watch_section/watch_component.dart
@@ -27,80 +27,11 @@ class _WatchComponentState extends State<WatchComponent> {
                     pro.searchController.text.isNotEmpty) ||
                 pro.appBarState == AppBarState.searchSubmit
             ? SearchSubmitted(moviesList: pro.filtered)
-            // ? Column(
-            //     crossAxisAlignment: CrossAxisAlignment.start,
-            //     children: [
-            //       30.spaceY,
-            //       Container(
-            //         margin: const EdgeInsets.only(left: 21),
-            //         child: Text('Top Result',
-            //             style: AppTextstyle.poppins(
-            //                 color: AppColors.darkBlue,
-            //                 fontWeight: FontWeight.w500,
-            //                 fontSize: 12)),
-            //       ),
-            //       const Divider(indent: 20, endIndent: 20),
-            //       Expanded(
-            //         child: ListView.builder(
-            //           itemCount:pro.filtered.length,
-            //           padding: const EdgeInsets.symmetric(horizontal: 20),
-            //           itemBuilder: (BuildContext context, int index) {
-            //             Results item=pro.filtered[index];
-            //             return  SearchMovieWidget(
-            //               onTap: () {
-            //                 Navigation.pushNamed(
-            //                     MovieDetailScreen.routeName,arguments: item.id);
-            //               },
-            //                 title: item.title??'',
-            //                 genre: item.title??"",
-            //                 imageUrl:AppConstants.imageUrl(item.posterPath ?? ''));
-            //           },
-            //         ),
-            //       ),
-            //     ],
-            //   )
+           
             : pro.appBarState == AppBarState.searchOpened
                 ? SearchOpened(moviesList: data.results ?? [])
                 : MovieCommon(moviesList: data.results ?? []);
-        // ? GridView.builder(
-        //     padding: const EdgeInsets.symmetric(
-        //         horizontal: 15, vertical: 15),
-        //     gridDelegate:
-        //         const SliverGridDelegateWithFixedCrossAxisCount(
-        //             crossAxisCount: 2,
-        //             childAspectRatio: 1.5,
-        //             crossAxisSpacing: 10,
-        //             mainAxisSpacing: 10),
-        //     itemCount: data.results?.length ?? 0,
-        //     itemBuilder: (context, index) {
-        //       Results item = data.results![index];
-        //       return MovieWidget(
-        //           onTap: () {
-        //             Navigation.pushNamed(
-        //                 MovieDetailScreen.routeName,arguments: item.id);
-        //           },
-        //           height: 80,
-        //           marginTop: 0,
-        //           title: item.title ?? '',
-        //           imageUrl:
-        //               AppConstants.imageUrl(item.posterPath ?? ''));
-        //     })
-        // : ListView.builder(
-        //     itemCount: data.results?.length ?? 0,
-        //     padding: const EdgeInsets.symmetric(
-        //         horizontal: 20, vertical: 15),
-        //     itemBuilder: (BuildContext context, int index) {
-        //       Results item = data.results![index];
-        //       return MovieWidget(
-        //           onTap: () {
-        //             Navigation.pushNamed(
-        //                 MovieDetailScreen.routeName,arguments: item.id);
-        //           },
-        //           title: item.title ?? '',
-        //           imageUrl:
-        //               AppConstants.imageUrl(item.posterPath ?? ''));
-        //     },
-        //   );
+       
       });
     });
   }
